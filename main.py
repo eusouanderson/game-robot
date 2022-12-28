50389af63f9f2a602ee3f4fad78d01ec214db0c7
Anderson Rodrigues <eusouanderson@outlook.com>
2022-12-28 14:00:08 -0300

    Player

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import sys, pygame
 
+
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__()
 
-        self.andar_animation = False
+
+        self.run_animation = False
+        self.runl_animation = False
         self.sprites = []
         self.sprites.append(pygame.image.load('png/Run (1).png'))
         self.sprites.append(pygame.image.load('png/Run (2).png'))
@@ -38,30 +41,37 @@ class Player(pygame.sprite.Sprite):
         self.current_sprite_idle = 0
 
         self.imageL = self.spritesL[self.current_sprite_idle]
-        self.rect = self.imageL.get_rect()
-        self.rect.topleft = [pos_x, pos_y]
+        self.rect1 = self.imageL.get_rect()
+        self.rect1.topleft = [pos_x, pos_y]
 
-    def andar(self):
-        self.andar_animation = True
+    def runL(self):
+        self.runl_animation = True
+
+    def run(self):
+        self.run_animation = True
 
     def idle(self):
         self.idle_animation = True
 
     def update(self, speed):
 
-        if self.andar_animation:
+        if self.run_animation:
             self.current_sprite += speed
             if int(self.current_sprite) >= len(self.sprites):
                 self.current_sprite = 0
-                self.andar_animation = False
+                'self.andar_animation = False'
 
         self.image = self.sprites[int(self.current_sprite)]
 
+        if self.runl_animation:
+            'self.img_with_flip = pygame.transform.flip(self.image, True, False)'
+
         if self.idle_animation:
+
             self.current_sprite_idle += speed
             if int(self.current_sprite_idle) >= len(self.spritesL):
                 self.current_sprite_idle = 0
-                self.idle_animation = False
+                'self.idle_animation = False'
 
         self.image = self.spritesL[int(self.current_sprite_idle)]
 
@@ -73,14 +83,12 @@ tamanho_da_tela = altura, largura
 screen = pygame.display.set_mode(tamanho_da_tela)
 pygame.display.set_caption('Game')
 color = 255, 255, 255
-
 moving_sprites = pygame.sprite.Group()
-
 player = Player(50, 50)
-
 moving_sprites.add(player)
 
 while True:
+    pygame.sprite.Group().update(player)
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
@@ -89,13 +97,17 @@ while True:
         if event.type == pygame.KEYDOWN:
 
             if event.key == pygame.K_d:
-                player.andar()
+                player.run()
 
             if event.key == pygame.K_s:
                 player.idle()
 
+            if event.key == pygame.K_a:
+                ' player.runL()'
+
     screen.fill(color)
     moving_sprites.draw(screen)
     moving_sprites.update(0.25)
+    'screen.blit(screen, (50 + 1 * 120, 100))'
     pygame.display.flip()
     clock.tick(60)
